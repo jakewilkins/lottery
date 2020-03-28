604d0197150870f8e81bf9cf1a480cbb23f7ae7f
Jake Wilkins <jakewilkins@github.com>
2020-03-27 22:23:13 -0700

    Scope interaction processor to account id

--- interaction_processor.rb
+++ interaction_processor.rb
@@ -10,17 +10,17 @@ module InteractionProcessor
     when "mark-sharing"
       mark_sharing(*payload.split("."))
     when "draw-again"
-      draw_again(payload)
+      draw_again(*payload.split("."))
     when "reset-shared"
-      reset_shared(payload)
+      reset_shared(*payload.split("."))
     end
     response.message_id = event["messageId"]
 
     response
   end
 
-  def mark_sharing(meeting_id, person_id)
-    meeting = Meeting.find(meeting_id)
+  def mark_sharing(account_id, meeting_id, person_id)
+    meeting = Meeting.find(meeting_id, account: account_id)
     return unless meeting.alive?
 
     meeting.shared(person: person_id)
@@ -28,8 +28,8 @@ module InteractionProcessor
     Response.person_sharing(meeting_id)
   end
 
-  def reset_shared(meeting_id)
-    meeting = Meeting.find(meeting_id)
+  def reset_shared(account_id, meeting_id)
+    meeting = Meeting.find(meeting_id, account: account_id)
     return unless meeting.alive?
 
     meeting.clear_shared
@@ -37,8 +37,8 @@ module InteractionProcessor
     Response.sharing_reset(meeting_id)
   end
 
-  def draw_again(meeting_id)
-    meeting = Meeting.find(meeting_id)
+  def draw_again(account_id, meeting_id)
+    meeting = Meeting.find(meeting_id, account: account_id)
     return unless meeting.alive?
 
     winner = meeting.draw
